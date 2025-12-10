4cc5007acc3c47dfc8d75e580d08fe4245559538
Cullen Wallace <cullenawallace@gmail.com>
2025-12-10 13:18:04 -0500

    add semi

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,6 +3,6 @@
  * docs: https://the-one-api.dev/documentation
  */
 
-export const API_BASE = 'https://the-one-api.dev/v2'
+export const API_BASE = 'https://the-one-api.dev/v2';
 export const API_KEY = '';
 
